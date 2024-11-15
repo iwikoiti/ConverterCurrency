bb4ab41f7191058d52f7863b35a366a59a152f82
yui <yuineor@gmail.com>
2024-11-16 02:15:30 +0700

    design mainActivity and create resultCurrency(second activity)

--- app/src/main/java/com/example/convertercurrency/resultCurrency.kt
+++ app/src/main/java/com/example/convertercurrency/resultCurrency.kt
@@ -1,31 +1,20 @@
 package com.example.convertercurrency
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import android.content.Intent
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var okButton: Button
-
+class resultCurrency : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_result_currency)
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
-        okButton = findViewById(R.id.okButton)
-
-        okButton.setOnClickListener{
-            val intent = Intent(this, resultCurrency::class.java)
-            startActivity(intent)
-        }
     }
 }
