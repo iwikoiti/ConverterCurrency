56a8367b1dca5916c68d861ddc398872c6e494cd
yui <yuineor@gmail.com>
2024-11-16 11:52:52 +0700

    init xml elements in main activity

--- app/src/main/java/com/example/convertercurrency/MainActivity.kt
+++ app/src/main/java/com/example/convertercurrency/MainActivity.kt
@@ -5,25 +5,38 @@ import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
+import android.widget.EditText
+import android.widget.RadioButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var okButton: Button
+    private lateinit var costEditText: EditText
+    private lateinit var radioButtonEuro: RadioButton
+    private lateinit var radioButtonDollar: RadioButton
+    private lateinit var radioButtonPound: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
+
         okButton = findViewById(R.id.okButton)
+        costEditText = findViewById(R.id.costEditText)
+        radioButtonEuro = findViewById(R.id.radioButtonEuro)
+        radioButtonDollar = findViewById(R.id.radioButtonDollar)
+        radioButtonPound = findViewById(R.id.radioButtonPound)
 
         okButton.setOnClickListener{
+
+            var cost = costEditText.text.toString().toFloatOrNull() ?: 0f
+
+
+
+
+
             val intent = Intent(this, resultCurrency::class.java)
             startActivity(intent)
         }
