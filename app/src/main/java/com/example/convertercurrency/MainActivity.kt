ccb78a05935cb74c9e8eeb9c07c86289baa565f9
yui <yuineor@gmail.com>
2024-11-16 12:26:32 +0700

    get currency values from radio group

--- app/src/main/java/com/example/convertercurrency/MainActivity.kt
+++ app/src/main/java/com/example/convertercurrency/MainActivity.kt
@@ -29,17 +29,26 @@ class MainActivity : AppCompatActivity() {
         okButton = findViewById(R.id.okButton)
         costEditText = findViewById(R.id.costEditText)
         radioGroupCurrency = findViewById(R.id.radioGroupCurrency)
-
-//        radioButtonEuro = findViewById(R.id.radioButtonEuro)
-//        radioButtonDollar = findViewById(R.id.radioButtonDollar)
-//        radioButtonPound = findViewById(R.id.radioButtonPound)
+        var convertValue = 0f
 
         radioGroupCurrency.setOnCheckedChangeListener { _, checkedId ->
             val selectedRadioButton = findViewById<RadioButton>(checkedId)
             val selectedCurrency = selectedRadioButton?.text.toString()
 
-            Toast.makeText(this, "Выбрана валюта: $selectedCurrency", Toast.LENGTH_SHORT).show()
-            // Можно сохранить выбранное значение в переменную
+            val w = getString(R.string.dollar_sign)
+            when (selectedCurrency) {
+                getString(R.string.euro_sign) -> {
+                    convertValue = 90f
+                }
+                getString(R.string.dollar_sign) -> {
+                    convertValue = 75f
+                }
+                getString(R.string.pound_sign) -> {
+                    convertValue = 100f
+                }
+            }
+            Toast.makeText(this, "Выбрана валюта: $convertValue", Toast.LENGTH_SHORT).show()
+
         }
 
         okButton.setOnClickListener{
